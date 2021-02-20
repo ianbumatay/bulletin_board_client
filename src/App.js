bdb70d065d7eda201215ca2a8080e6f893c4741c
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-20 12:36:27 -0500

    change app component to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+import React, { Component } from 'react'
+
+export class App extends Component { 
+
+
+// componentDidMount(){
+//   fetch("http://localhost:3000/api/v1/boards") 
+//   .then(res => res.json())
+//   .then(boards => console.log(boards))
+// }
+
+
+
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
