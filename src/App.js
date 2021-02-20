7e9db08415665f6122a09a3493d5935547fdc229
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-20 13:41:22 -0500

    install all my dependencies

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ export class App extends Component {
 
 
 // componentDidMount(){
-//   fetch("http://localhost:3000/api/v1/boards") 
+//   fetch("http://localhost:3000/api/v1/boards/1") 
+//   //fetch("http://localhost:3000/api/v1/boards/1/bulletins") 
 //   .then(res => res.json())
 //   .then(boards => console.log(boards))
 // }
