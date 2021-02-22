2e42fc82cef07efd3c803e883a78e253b7b2a4f7
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-22 12:56:25 -0500

    build fetchBoard action creator

--- src/actions/fetchBoards.js
+++ src/actions/fetchBoards.js
@@ -1,3 +1,10 @@
 export  const fetchBoards = () => {
-    
+    return (dispatch) => {
+       fetch( 'http://localhost:3000/api/v1/boards' ) 
+       .then(res => res.json())
+       .then(boards => dispatch({
+           type: FETCH_BOARDS, 
+           payload: boards
+       }))
+    }   
 }
