d5d7c588df13f9cd39823064707cb3b8c6481b3d
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-22 22:04:26 -0500

    created hanlde change fuction and set up controlled component

--- src/components/BoardForm.js
+++ src/components/BoardForm.js
@@ -1,19 +1,31 @@
 
 
 
-import React, { Component } from 'react'
+import React, { Component } from 'react' 
+
 
 export class BoardForm extends Component {
 
 
-    
+constructor(props) {
+    super(props)
+
+    this.state = {
+         name: " "
+    }
+}
+
+
+ handleChange = event => {
+    this.setState({name: event.target.value})
+}
+ 
     render() {
         return (
             <div>
                 <form>
                     <label> Create boards </label>
-                    <input type="text" name="name" // value={this.state.name} 
-                    />
+                    <input type="text" name="name" onChange={this.handleChange} value={this.state.name}/>
                     <input type="submit"/>
                 </form>
 
