ec2f389290cdb68caa76caeb3d6ef237b0663a4a
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-02-25 08:25:06 -0500

    created navBar and home component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className="homeDiv">
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
